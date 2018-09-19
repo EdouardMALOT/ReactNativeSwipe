30c2a09633f45e7c9071847bdfccf11a0622300e
Edouard MALOT <edouard.malot@gmail.com>
2018-09-19 12:28:21 +0700

    First Gesture/animation

--- src/Deck.js
+++ src/Deck.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { 
-  View, 
+  View,
+  Animated,
   PanResponder
 } from 'react-native';
 
@@ -8,23 +9,30 @@ class Deck extends Component {
 
   constructor(props) {
     super(props);
+
+    const position = new Animated.ValueXY();
+
     const panResponder = PanResponder.create({
       onStartShouldSetPanResponder: () => true,
       onPanResponderMove: (event, gesture) => { 
-        console.log(gesture); 
+       position.setValue({ x: gesture.dx, y: gesture.dy });
       },
       onPanResponderRelease: (event, gesture) => { console.log(gesture); }
     });
 
-    this.state = { panResponder };
+    this.state = { panResponder, position };
   }
 
   renderCards() {
     return this.props.data.map(item => {
       return (
-        <View key={item.id} {...this.state.panResponder.panHandlers}>
+        <Animated.View 
+          key={item.id} 
+          style={this.state.position.getLayout()}
+          {...this.state.panResponder.panHandlers}
+        >
          {this.props.renderCard(item)}
-        </View> 
+        </Animated.View> 
       );
     });
   }
