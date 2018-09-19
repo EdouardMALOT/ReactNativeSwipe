4f94a643dbffa268c447f5273ecbf742d095a6b3
Edouard MALOT <edouardmalot@MacBook-Pro-de-Edouard.local>
2018-09-19 11:49:56 +0700

    App start working

---
+++ src/Deck.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import { View } from 'react-native';
+
+class Deck extends Component {
+
+  renderCards() {
+    return this.props.data.map(item => {
+      return (
+        <View key={item.id}>
+         {this.props.renderCard(item)}
+        </View> 
+      );
+    });
+  }
+
+  render() {
+    return (
+      <View>
+        {this.renderCards()}
+      </View>
+    );
+  }
+}
+
+export default Deck;
