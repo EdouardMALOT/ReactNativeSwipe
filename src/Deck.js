f2b23feadd72d401548f2e5e9f72bf2c42b71427
Edouard MALOT <edouardmalot@MacBook-Pro-de-Edouard.local>
2018-09-19 12:14:53 +0700

    Add panResponder

--- src/Deck.js
+++ src/Deck.js
@@ -1,12 +1,28 @@
 import React, { Component } from 'react';
-import { View } from 'react-native';
+import { 
+  View, 
+  PanResponder
+} from 'react-native';
 
 class Deck extends Component {
+
+  constructor(props) {
+    super(props);
+    const panResponder = PanResponder.create({
+      onStartShouldSetPanResponder: () => true,
+      onPanResponderMove: (event, gesture) => { 
+        console.log(gesture); 
+      },
+      onPanResponderRelease: (event, gesture) => { console.log(gesture); }
+    });
+
+    this.state = { panResponder };
+  }
 
   renderCards() {
     return this.props.data.map(item => {
       return (
-        <View key={item.id}>
+        <View key={item.id} {...this.state.panResponder.panHandlers}>
          {this.props.renderCard(item)}
         </View> 
       );
